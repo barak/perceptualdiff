3b73e8c8fbac9e41180c8da6f95d3e8e8493400f
Simon McVittie <smcv@ianadd.pseudorandom.co.uk>
2007-10-14 12:24:05 +0100

    Imported Debian patch 1.0.1-1.1

--- CMakeFiles/perceptualdiff.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE
-"perceptualdiff"
-"CMakeFiles/perceptualdiff.dir/PerceptualDiff.o"
-"CMakeFiles/perceptualdiff.dir/LPyramid.o"
-"CMakeFiles/perceptualdiff.dir/RGBAImage.o"
-"CMakeFiles/perceptualdiff.dir/CompareArgs.o"
-"CMakeFiles/perceptualdiff.dir/Metric.o"
-)
