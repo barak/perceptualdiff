f7a5be90986890bd2b38233fc17fb6b062c5a905
Barak A. Pearlmutter <barak+git@pearlmutter.net>
2015-09-09 16:00:22 +0100

    sync to upstream/1.2
    rm CTestTestfile.cmake
    rm install_manifest.txt
    
    These stray files were not in the upstream/1.2 and are not needed for
    the build.  Removing them does no hard, and serves to confine all
    differences from upstream/1.2 to debian/*.

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /tmp/pd/perceptualdiff-1.2
-# Build directory: /tmp/pd/perceptualdiff-1.2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_tests.bash, "/tmp/pd/perceptualdiff-1.2/test/run_tests.bash")
